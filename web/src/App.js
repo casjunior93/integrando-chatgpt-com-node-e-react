71682e7ac1988e5b768af589e31a243b0cd8dccc
Carlos Alberto Silva Junior <casjunior93@gmail.com>
2023-07-09 10:48:57 -0300

    first commit

---
+++ web/src/App.js
@@ -0,0 +1,18 @@
+import './styles/App.css';
+import './styles/reset.css';
+
+import {makeRequest} from './api/api';
+
+import{useState} from 'react';
+import {SideMenu} from './components/SideMenu/SideMenu';
+
+function App() {
+  return (
+    <div className="App">
+      <SideMenu></SideMenu>
+      <h1>Trabalhando!!</h1>
+    </div>
+  );
+}
+
+export default App;
